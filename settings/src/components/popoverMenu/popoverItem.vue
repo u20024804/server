b5646d8808b2c1a9d8c82ad95762cee82725fe6f
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2018-06-07 16:23:39 +0200

    Popover design fixes and settings update
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- settings/src/components/popoverMenu/popoverItem.vue
+++ settings/src/components/popoverMenu/popoverItem.vue
@@ -1,7 +1,7 @@
 <template>
 	<li>
 		<!-- If item.href is set, a link will be directly used -->
-		<a @click="item.action" v-if="item.href" :href="(item.href) ? item.href : '#' ">
+		<a @click="item.action" v-if="item.href" :href="(item.href) ? item.href : '#' " :target="(item.target) ? item.target : '' " rel="noreferrer noopener">
 			<span :class="item.icon"></span>
 			<span v-if="item.text">{{item.text}}</span>
 			<p v-else-if="item.longtext">{{item.longtext}}</p>
@@ -13,7 +13,7 @@
 			<p v-else-if="item.longtext">{{item.longtext}}</p>
 		</button>
 		<!-- If item.longtext is set AND the item does not have an action -->
-		<span v-else>
+		<span class="menuitem" v-else>
 			<span :class="item.icon"></span>
 			<span v-if="item.text">{{item.text}}</span>
 			<p v-else-if="item.longtext">{{item.longtext}}</p>
